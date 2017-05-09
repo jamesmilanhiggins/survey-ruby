332d35584f22836a804cea8df9a2eb8672fcd99f
James Higgins and Kai de Rochemont <james@>
2017-05-09 09:45:27 -0700

    Add make title case method and require before save

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,3 +1,11 @@
 class Survey < ActiveRecord::Base
   has_many(:contents)
+  before_save :make_title_case
+
+  private
+
+  def make_title_case
+    self.title = self.title.titlecase
+  end
+
 end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -1,11 +1,21 @@
 require "spec_helper"
 
 describe 'Survey' do
+
   describe '#contents' do
     it('tells which contents are assigned to the survey') do
       test_survey = Survey.create(title: "colors survey")
       test_content = Content.create(question: "What is your favorite color?", survey_id: test_survey.id)
-      expect(test_survey.contents).to eq([test_content])
+      test_content2 = Content.create(question: "What is least favorite color?", survey_id: test_survey.id)
+      expect(test_survey.contents).to eq([test_content, test_content2])
     end
   end
+
+  describe '#create' do
+    it('tells which contents are assigned to the survey') do
+      test_survey = Survey.create(title: "colors survey")
+      expect(test_survey.title).to eq('Colors Survey')
+    end
+  end
+
 end
