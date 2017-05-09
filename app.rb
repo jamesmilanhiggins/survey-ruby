f3e641782448139f7d78b141b7e36e8e9e985c9d
James Higgins and Kai de Rochemont <james@>
2017-05-09 11:59:55 -0700

    create route to submit survey

--- app.rb
+++ app.rb
@@ -5,9 +5,41 @@ require "./lib/survey"
 require "./lib/question"
 require "./lib/result"
 require "pry"
+enable :sessions
 
 also_reload "lib/**/*.rb"
 
-get "/" do
+get '/' do
+  @all_surveys = Survey.all
   erb :index
+end
+
+get '/surveys/new' do
+  erb :new_survey
+end
+
+post '/surveys/new' do
+  survey_title = params['survey-title']
+  new_survey = Survey.create(title: survey_title)
+  redirect "/surveys/#{new_survey.id}"
+end
+
+get '/surveys/:id' do
+  survey_id = params['id']
+  @survey = Survey.find(survey_id)
+  erb :survey
+end
+
+post '/surveys/:id/submit' do
+  survey_id = params['id']
+  @survey = Survey.find(survey_id)
+  binding.pry
+  erb :survey
+end
+
+post '/surveys/:id/add-question' do
+  question = params['question']
+  survey_id = params['id']
+  new_question = Question.create(content: question, survey_id: survey_id)
+  redirect "/surveys/#{survey_id}"
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'Question' do
+
   describe '#survey' do
     it('tells which survey the question is asscociated with') do
       test_survey = Survey.create(title: "colors survey")
@@ -8,4 +9,15 @@ describe 'Question' do
       expect(test_question.survey).to eq(test_survey)
     end
   end
+
+  describe '#results' do
+    it('tells which results are asscociated with the question') do
+      test_survey = Survey.create(title: "colors survey")
+      test_question = Question.create(content: "What is your favorite color?", survey_id: test_survey.id)
+      test_result = Result.create(answer: "red", survey_id: test_survey.id, question_id: test_question.id)
+      test_result2 = Result.create(answer: "grey", survey_id: test_survey.id, question_id: test_question.id)
+      expect(test_question.results).to eq([test_result, test_result2])
+    end
+  end
+
 end
